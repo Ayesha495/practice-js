90c20c8d6edb53993e0491f121050c0dda617f53
Dreambackie <tabassuma564@gmail.com>
2025-02-20 09:45:54 +0500

    task1

---
+++ task1.js
@@ -0,0 +1,13 @@
+
+var course = 'MAD';
+function greet(name, age) {
+    let address = "Kahuta";
+    console.log(`${course}`);
+    console.log(`My name is ${name} I live in ${address} and I am ${age} years old` );
+
+}
+greet("Ayesha", 19);
+
+console.log(`${address} ${course}`);
+
+ 
