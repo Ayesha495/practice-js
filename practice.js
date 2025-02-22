9d01835db2ec3ec2e8e1a481124c4b5704ec8376
Ayesha495 <ayescapade@gmail.com>
2025-02-22 23:34:21 +0500

    practice closures

--- practice.js
+++ practice.js
@@ -1,9 +1,10 @@
 function upper(){
     let counter = 0;
-     function inner() {
+    function inner() {
         counter++;
-        console.log(counter)
-     }
-     inner();
-} 
-upper();
+        console.log(counter);
+    }
+    return inner;  // Return inner, don't call iit, return it!!!!!!!!!
+    // const fn = upper();  // Now fn is assigned the inner function
+fn(); // 1
+fn(); // 2
